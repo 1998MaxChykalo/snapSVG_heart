c288e244b201249ff712325122ae5c1fce3e4685
1998MaxChykalo <maxymchykalo@gmail.com>
2018-05-01 01:32:31 +0300

    initial commit

---
+++ docs/main.js
@@ -0,0 +1,41 @@
+
+// var paper = Snap('#preloader__logo');
+
+// var renderNodes = function(i) {
+//   var node = paper.circle(x, y, r);
+//   node.attr({
+//     fill: '#fff'
+//   })
+// }
+
+// var drawPath = function(i) {
+//   var path = paper
+//               .path(`M ${pos[i].x}, ${pos[i].y}`)
+//               .attr({
+//                 fill: 'transparent',
+//                 stroke: '#fff',
+//                 strokeWidth: 1
+//               })
+//               .animate({
+//                 d: `M ${pos[i].x}, ${pos[i].y} L ${pos[i+1].x}, ${pos[i+1].y}`,
+//               },
+//               1000,
+//               mina.easeinout
+//             )
+// };
+
+// var pos = [
+//   {
+//     x: 133,
+//     y: 107,
+//   },
+//   {
+//     x: 202,
+//     y: 106,
+//   },
+  
+// ]
+
+// var i = 0;
+
+// drawPath(i);
